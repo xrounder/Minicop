a8b5dc706f0b350da2d89dce721b63dbf598d19f
Dennis <d.czogalla@gmx.de>
2018-06-18 11:21:02 +0200

    update

--- gnn/RecurrentNeuralNetV2.py
+++ gnn/RecurrentNeuralNetV2.py
@@ -8,7 +8,7 @@ w12 = -10
 w22 = 0
 o1 = 0.770719
 o2 = 0.00978897
-epsilon = 0.0001
+epsilon = 0.00001
 
 def sigmoid(x):
     return 1/(1+math.exp(-x))
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     input2 = 0
 
     x = 0
-    scalefactor = 0.5
+    scalefactor = 1
 
     copyInput1 = 1
     copyInput2 = 1
@@ -31,12 +31,12 @@ if __name__ == "__main__":
     #input1 = sigmoid(copyInput1 * w11 * scalefactor + copyInput2 * w21 * scalefactor + bias1)
     #input2 = sigmoid(copyInput2 * w22 * scalefactor + copyInput1 * w12 * scalefactor + bias2)
 
-    while math.fabs(copyInput1 - input1) > epsilon and math.fabs(copyInput2 - input2) > epsilon:
+    while math.fabs(copyInput1 - input1) > epsilon or math.fabs(copyInput2 - input2) > epsilon:
 
         input1 = sigmoid(o1 * w11 + o2 * w21 + bias1)
         input2 = sigmoid(o2 * w22 + o1 * w12 + bias2)
 
-        while x < 5:
+        while x < 3:
 
             copyInput1 = input1
             copyInput2 = input2
@@ -46,8 +46,11 @@ if __name__ == "__main__":
             print(input1, input2)
             x+=1
 
+        #print(copyInput1,copyInput2)
         #print(input1, input2)
+        print("scalefactor: ",scalefactor)
         x = 0
+        print("condition: ",math.fabs(copyInput1 - input1), math.fabs(copyInput2 - input2))
         print("==============")
 
-        scalefactor += 0.01
+        scalefactor -= epsilon
