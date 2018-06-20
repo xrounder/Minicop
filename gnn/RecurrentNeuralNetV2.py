b5affa4a4631c0d25c380814c361ebf7589a8510
xrounder Miguel Caballero <1414163@stud.hs-mannheim.de>
2018-06-20 18:35:45 +0200

    nnummer 6 OMGgit status!

--- gnn/RecurrentNeuralNetV2.py
+++ gnn/RecurrentNeuralNetV2.py
@@ -1,56 +1,60 @@
 import math
+import numpy
 
 bias1 = -4
 bias2 = 4
-w11 = 6
-w21 = 10
-w12 = -10
-w22 = 0
+w11 = 6.0
+w21 = 10.0
+w12 = -10.0
+w22 = 0.0
+wMatrix = numpy.array([[w11, w12], [w21, w22]])
 o1 = 0.770719
 o2 = 0.00978897
-epsilon = 0.00001
+epsilon = 0.001
+
 
 def sigmoid(x):
-    return 1/(1+math.exp(-x))
+    return 1 / (1 + math.exp(-x))
 
 
 if __name__ == "__main__":
 
-    #input1 = sigmoid(o1*w11+o2*w21+bias1)
-    #input2 = sigmoid(o2*w22+o1*w12+bias2)
-    #print(input1,input2)
     input1 = 0
     input2 = 0
 
     x = 0
-    scalefactor = 1
+    scalefactor = 1.0
 
     copyInput1 = 1
     copyInput2 = 1
+    eigenvalues = numpy.array(numpy.real(numpy.linalg.eigvals(wMatrix * scalefactor)))
 
-    #input1 = sigmoid(copyInput1 * w11 * scalefactor + copyInput2 * w21 * scalefactor + bias1)
-    #input2 = sigmoid(copyInput2 * w22 * scalefactor + copyInput1 * w12 * scalefactor + bias2)
+    while math.fabs(copyInput1 - input1) > epsilon or math.fabs(copyInput2 - input2) > epsilon or math.fabs(
+            eigenvalues[0] - 1) >= epsilon or math.fabs(
+            eigenvalues[1] - 1) >= epsilon:
 
-    while math.fabs(copyInput1 - input1) > epsilon or math.fabs(copyInput2 - input2) > epsilon:
+        input1 = sigmoid(o1 * wMatrix[0][0] + o2 * wMatrix[1][0] + bias1)
+        input2 = sigmoid(o2 * wMatrix[1][1] + o1 * wMatrix[0][1] + bias2)
 
-        input1 = sigmoid(o1 * w11 + o2 * w21 + bias1)
-        input2 = sigmoid(o2 * w22 + o1 * w12 + bias2)
-
-        while x < 3:
-
+        while x < 100:
             copyInput1 = input1
             copyInput2 = input2
-            input1 = sigmoid(copyInput1*w11*scalefactor+copyInput2*w21*scalefactor+bias1)
-            input2 = sigmoid(copyInput2*w22*scalefactor+copyInput1*w12*scalefactor+bias2)
 
-            print(input1, input2)
-            x+=1
+            input1 = sigmoid(copyInput1 * wMatrix[0][0] * scalefactor + copyInput2 * wMatrix[1][0] * scalefactor + bias1)
+            input2 = sigmoid(copyInput2 * wMatrix[1][1] * scalefactor + copyInput1 * wMatrix[0][1] * scalefactor + bias2)
 
-        #print(copyInput1,copyInput2)
-        #print(input1, input2)
-        print("scalefactor: ",scalefactor)
+            # print(input1, input2)
+            x += 1
+
+        print(copyInput1, copyInput2)
+        print(input1, input2)
+        print("scalefactor: ", scalefactor)
         x = 0
-        print("condition: ",math.fabs(copyInput1 - input1), math.fabs(copyInput2 - input2))
+        print("condition: ", math.fabs(copyInput1 - input1), math.fabs(copyInput2 - input2))
+        print("eigenwert: ", eigenvalues)
         print("==============")
-
-        scalefactor -= epsilon
+        eigenvalues = numpy.array(numpy.real(numpy.linalg.eigvals(wMatrix * scalefactor)))
+        scalefactor -= 0.0001
+    print("stop! Hammertime")
+    print("Eigenwerte: ", eigenvalues)
+    print("Gewichte: ", wMatrix * scalefactor)
